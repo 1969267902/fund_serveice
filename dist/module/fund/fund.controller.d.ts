3332a150d7226d15c0486b66b55c8d66a032e078
guoqian <guoqian@pillarbiosci.com>
2023-08-07 20:19:29 +0800

    增加基金模块

---
+++ dist/module/fund/fund.controller.d.ts
@@ -0,0 +1,3 @@
+export declare class FundController {
+    findAll(): string;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FundController } from './module/fund/fund.controller';
+import { FundService } from './module/fund/fund.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, FundController],
+  providers: [AppService, FundService],
 })
 export class AppModule {}
